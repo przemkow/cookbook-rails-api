99978b8fad9fcd7d8c95aa1c89598ac049b62d9c
przemkow <przemkow92@gmail.com>
2016-02-15 02:30:26 +0100

    add authentication token to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214221311) do
+ActiveRecord::Schema.define(version: 20160215012609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,10 @@ ActiveRecord::Schema.define(version: 20160214221311) do
     t.string   "first_name",             default: ""
     t.string   "last_name",              default: ""
     t.text     "about",                  default: ""
+    t.string   "auth_token"
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
