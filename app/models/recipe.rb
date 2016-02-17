ba07b8ed3c6555e5aa1215ddc0be69d9c611a5d9
przemkow <przemkow92@gmail.com>
2016-02-17 01:08:11 +0100

    set recipe validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
-
   has_and_belongs_to_many :followers, class_name: 'User', join_table: 'favourites_followers', association_foreign_key: 'user_id'
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :preparation_time_in_sec, presence: true
 end
